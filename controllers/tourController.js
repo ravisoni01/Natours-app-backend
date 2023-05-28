b0e0e99764ea55c153aef1cd5327ecae73577732
Ravi Soni <r7679242@gmail.com>
2023-05-28 18:51:42 +0530

    add sorting , filter , field limit , pagination , alias feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,9 +10,59 @@ exports.checkId = (req, res, next, val) => {
   next();
 };
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingAverage,price";
+  req.query.fields = "name,price,ratingAverage,summary,difficulty";
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build query
+    // 1) Filtering
+    let queryObj = { ...req.query };
+    let excludedFields = ["page", "limit", "sort", "fields"];
+    excludedFields.forEach((el) => {
+      delete queryObj[el];
+    });
+
+    // 2) Advance filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 3) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // 4) Field linit
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // 5) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
+
+    // Execute query
+    const tours = await query;
 
     res.status(200).send({
       status: "success",
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,12 +7,14 @@ const {
   deleteTour,
   checkId,
   checkBody,
+  aliasTopTours,
 } = require("../controllers/tourController");
 
 const router = express.Router();
 
 router.param("id", checkId);
 
+router.route("/top-5-cheap").get(aliasTopTours, getAllTours);
 router.route("/").get(getAllTours).post(createTour);
 
 router.route("/:id").get(singleTour).patch(updateTour).delete(deleteTour);
